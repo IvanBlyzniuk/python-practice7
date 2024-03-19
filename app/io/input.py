0e17310c7bd4e88d888e2cea39ff75a3a78e7f3b
IvanBlyzniuk <84847629+IvanBlyzniuk@users.noreply.github.com>
2024-03-19 21:13:39 +0200

    + docstrings for io functions ~ fixed signature of output functions

--- app/io/input.py
+++ app/io/input.py
@@ -1,12 +1,30 @@
 
 
 def input_from_console():
+    """
+    Reads some input from console and returns it as a string
+
+    Returns:
+        str. The string that was inputted in console
+    """
     pass
 
 
 def input_from_file():
+    """
+    Reads some input from file named "input.txt" using default python capabilities
+
+    Returns:
+        str. The text that was present inside the file
+    """
     pass
 
 
 def input_from_file_pandas():
+    """
+    Reads some input from file named "input_pandas.txt" using pandas library
+
+    Returns:
+        str. The text that was present inside the file
+    """
     pass
--- app/io/output.py
+++ app/io/output.py
@@ -1,12 +1,30 @@
 
 
-def output_to_console():
+def output_to_console(text):
+    """
+    Outputs the given text to the console
+
+    Args:
+        text (str): the text that needs to be outputted
+    """
     pass
 
 
-def output_to_file():
+def output_to_file(text):
+    """
+    Outputs the given text to the file named "output.txt" using default python capabilities
+
+    Args:
+        text (str): the text that needs to be outputted
+    """
     pass
 
 
-def output_to_file_pandas():
+def output_to_file_pandas(text):
+    """
+    Outputs the given text to the file named "output_pandas.txt" using pandas library
+
+    Args:
+        text (str): the text that needs to be outputted
+    """
     pass
